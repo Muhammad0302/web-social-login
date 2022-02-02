c65f092644b80aab15e73dfb56beaca803f5fbd1
Muhammad Ismail <60970076+Muhammad0302@users.noreply.github.com>
2022-02-03 00:00:00 +0500

    Web socil-login

--- src/App.js
+++ src/App.js
@@ -1,41 +1,58 @@
-import './App.css';
-import { useState, useEffect } from 'react';
+import "./App.css";
+import { useState, useEffect } from "react";
 
-import { supabase } from './client';
+import { supabase } from "./client";
 
 function App() {
   const [user, setUser] = useState(null);
   useEffect(() => {
     checkUser();
-    window.addEventListener('hashchange', function() {
+    window.addEventListener("hashchange", function () {
       checkUser();
     });
-  }, [])
+  }, []);
   async function checkUser() {
     const user = supabase.auth.user();
     setUser(user);
   }
-  async function signInWithGithub() {
-    await supabase.auth.signIn({
-      provider: 'github'
+  async function signInWithFacebook() {
+    const { user, session, error } = await supabase.auth.signIn({
+      provider: "facebook",
     });
   }
-  async function signOut() {
-    await supabase.auth.signOut();
+  async function signInWithGoogle() {
+    const { user, session, error } = await supabase.auth.signIn({
+      provider: "google",
+    });
+  }
+  async function signInWithGithub() {
+    const { user, session, error } = await supabase.auth.signIn({
+      provider: "github",
+    });
+  }
+  // async function signOut() {
+  //   await supabase.auth.signOut();
+  //   setUser(null);
+  // }
+  async function signout() {
+    const { error } = await supabase.auth.signOut();
     setUser(null);
   }
   if (user) {
+    console.log(user);
     return (
       <div className="App">
         <h1>Hello, {user.email}</h1>
-        <button onClick={signOut}>Sign out</button>
+        <button onClick={signout}>Sign out</button>
       </div>
-    )
+    );
   }
   return (
     <div className="App">
       <h1>Hello, please sign in!</h1>
-      <button onClick={signInWithGithub}>Sign In</button>
+      <button onClick={signInWithGoogle}>Sign In with google</button>
+      <button onClick={signInWithFacebook}>Sign In with facebook</button>
+      <button onClick={signInWithGithub}>Sign In with github</button>
     </div>
   );
 }
--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient('https://app-id.supabase.co', 'anon-public-key')
+const supabase = createClient('https://xancuuyabymhlrhtquao.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0Mzc4Njc4MSwiZXhwIjoxOTU5MzYyNzgxfQ.qCcYN64iC-uYTt1MvVbn9C0s8lY0TkCz1jATX-6kvjs')
 
 export {
   supabase
